402ee2939c1019326abad4094b196f5ad2eb038b
Kristians Jansons <kristians.jansons@accenture.com>
2025-10-24 13:14:12 +0300

    Completed by creating a setValue that updates the value on button click as well as displays the correct value in the console

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  // return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  return (
+    <>
+      <h2>useState "gotcha"</h2>
+      <h3>{value}</h3>
+      <button
+        className="btn"
+        onClick={() => {
+          // value + 1;
+          // console.log(value + 1); Logged value is stale here
+          setValue((currentState) => {
+            const newValue = currentState + 1;
+            console.log(newValue);
+            return newValue;
+          });
+        }}
+      >
+        Increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
