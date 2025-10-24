0c112d8744435d8d1cb664687389271ab9c8dcb4
Kristians Jansons <kristians.jansons@accenture.com>
2025-10-24 12:46:09 +0300

    Completed this task independantly, will use the video if I find myself stuck or unsure. Otherwise, I will continue this way, comparing the starter file to the final file.

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,45 @@
-const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+import React from "react";
+import { data } from "../../../data";
 
+const UseStateArray = () => {
+  // return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  const clearAll = () => {
+    setPeople([]);
+  };
+
+  const resetAll = () => {
+    setPeople(data);
+  };
+
+  return (
+    <>
+      <h2>useState array example</h2>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removeItem(id)}>
+              Remove
+            </button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={clearAll}>
+        Clear Items
+      </button>
+      <div />
+      <button className="btn" onClick={resetAll}>
+        Reset Items
+      </button>
+    </>
+  );
+};
 export default UseStateArray;
