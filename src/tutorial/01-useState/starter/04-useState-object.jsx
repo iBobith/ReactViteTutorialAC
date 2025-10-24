491bfbbe3d2c8906d81874f1842506a1c7a12b43
Kristians Jansons <kristians.jansons@accenture.com>
2025-10-24 12:56:54 +0300

    Completed this both without using Batching and with

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,37 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // return <h2>useState object example</h2>;
+
+  // Before batching
+  // const [name, setName] = useState("Kris");
+  // const [age, setAge] = useState(20);
+  // const [hobby, setHobby] = useState("Coding");
+
+  // After batching
+  const [person, setPerson] = useState({
+    name: "Kris",
+    age: 20,
+    hobby: "Coding",
+  });
+
+  const handleClick = () => {
+    setPerson({ name: "Quentin", age: 45, hobby: "Ballet" });
+  };
+
+  return (
+    <>
+      <h2>useState object example</h2>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>
+        {person.name} likes {person.hobby}
+      </h3>
+      <button onClick={handleClick} className="btn">
+        Change
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
