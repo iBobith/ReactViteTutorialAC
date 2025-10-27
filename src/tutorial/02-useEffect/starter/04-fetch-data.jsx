01ec1aaa7d6f9a96dca5ee2a7f775128c2ad91ac
Kristians Jansons <kristians.jansons@accenture.com>
2025-10-27 15:03:06 +0200

    Completed the fetch data challenge - I successfully used the provided url and fetched the data and displayed it as requested in TUTORIAL.md using .then as opposed to async

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,39 @@
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
+import { useState, useEffect } from "react";
 
+// First I initialize a state variable to hold the url data (fetched users)
+// Then I use useEffect to fetch the data from the url
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((users) => setUsers(users))
+      .catch((error) => console.log(error));
+  });
+
+  // Then I render the fetched data
+  return (
+    <>
+      <h2>fetch data example</h2>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url} target="_blank" rel="noreferrer">
+                  profile
+                </a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 export default FetchData;
