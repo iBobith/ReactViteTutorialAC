223dfec868eca6fdc5b295adcfdae0e90a78927e
Kristians Jansons <kristians.jansons@accenture.com>
2025-10-27 15:46:24 +0200

    Completed the cleanup challenge for toggling a component and then cleaning it up

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <>
+      <h2>cleanup function</h2>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        toggle component
+      </button>
+      {toggle && <ToggleComponent />}
+    </>
+  );
+};
+const ToggleComponent = () => {
+  useEffect(() => {
+    console.log("toggle component mounted");
+  }, []);
+  return (
+    <div>
+      <h4>toggled component</h4>
+    </div>
+  );
 };
 
 export default CleanupFunction;
